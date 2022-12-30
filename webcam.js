d6e2038403e6329fda1f0145f6f177996bbd2c6d
jheinath <julianheinath@gmail.com>
2022-12-30 23:44:26 +0000

    Deploying to gh-pages from  @ cd231ecb702bf916448eb4f589258e68e63efa26 🚀

---
+++ webcam.js
@@ -0,0 +1,39 @@
+﻿function startVideo(src) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            let video = document.getElementById(src);
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            //mirror image
+            video.style.webkitTransform = "scaleX(-1)";
+            video.style.transform = "scaleX(-1)";
+        });
+    }
+}
+
+function getFrame(src, dest, dotNetHelper) {
+    let video = document.getElementById(src);
+    let canvas = document.getElementById(dest);
+    canvas.getContext('2d').drawImage(video, 0, 0, 320, 240);
+
+    let dataUrl = canvas.toDataURL("image/jpeg");
+    dotNetHelper.invokeMethodAsync('ProcessImage', dataUrl);
+}
+
+function stopVideo(src) {
+    let video = document.getElementById(src);
+    if (video && "srcObject" in video) {
+        const stream = video.srcObject;
+        if (stream) {
+            const tracks = stream.getTracks();
+            tracks.forEach(track => track.stop());
+        }
+        video.srcObject = null;
+    }
+}
