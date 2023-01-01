d4ec6a6303fac809b2909ebad06461bb0c4573ff
jheinath <julianheinath@gmail.com>
2023-01-01 22:57:35 +0100

    create javascript scan feature

---
+++ wwwroot/scan.js
@@ -0,0 +1,15 @@
+﻿function scan() {
+    import('./qr-scanner.min.js').then((module) => {
+        const QrScanner = module.default;
+        // do something with QrScanner
+        const videoElem = document.getElementById('video');
+        console.log('videoElem found')
+        if (videoElem === null) return;
+        const qrScanner = new QrScanner(
+            videoElem,
+            r => window.open(r.data, "_self"),
+            {returnDetailedScanResult: true}
+        );
+        qrScanner.start()
+    });
+}
