bb22a6743b618a55af20f8c72981ec251d6589ff
jheinath <julianheinath@gmail.com>
2023-01-04 15:48:23 +0100

    implement basic qr code generation with qrcode.js

---
+++ Application/Commands/CreateGameCommand.cs
@@ -0,0 +1,18 @@
+﻿using Application.Enums;
+using FluentResults;
+
+namespace Application.Commands;
+
+public class CreateGameCommand
+{
+    public CreateGameCommand()
+    {
+        
+    }
+
+    public Result Execute(Language language, IEnumerable<string> riddleSolutions)
+    {
+        var result = new Result();
+        return result;
+    }
+}
---
+++ Application/Enums/Language.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Enums;
+
+public enum Language
+{
+    English,
+    German
+}
